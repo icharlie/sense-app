a1d9d4bc28b19e74b60f503240361c9df7a6319b
Charlie Chang <icharlie.osu@gmail.com>
2018-02-01 01:17:33 -0800

    Search support.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+//
+const searchInPage = require('electron-in-page-search').default
+const electron = require('electron')
+const {remote} = electron
+const Mousetrap = require('mousetrap')
+const inPageSearch = searchInPage(remote.getCurrentWebContents());
+
+
+
+Mousetrap.bind([ 'command+f', 'ctrl+f' ], () => {
+  inPageSearch.openSearchWindow()
+})
